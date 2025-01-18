ab66df24d4f1ebc086a7f4cf1ee99f02c81ccde6
Bismay Bibhu Prakash <bismaybibhuprakash@gmail.com>
2025-01-18 19:43:42 +0530

    Add cookie-parser and cors middleware; implement error and response handling utilities

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import dotenv from 'dotenv'
-import connectDB  from "./db/index.js"
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
-    path: "./env"
-})
+  path: "./env",
+});
 
 connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on PORT: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`ERRR: Error conneting the database. ${error}`);
+  });
