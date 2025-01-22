c882366b6bb6862c243ec045acec0aafda0a5f1a
Bismay Bibhu Prakash <bismaybibhuprakash@gmail.com>
2025-01-22 19:30:02 +0530

    Implement user registration route and controller; add multer for file uploads and cloudinary configuration

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,23 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadOnCloudinary = async (filePath) => {
+  try {
+    if (!fs.existsSync(filePath)) return null;
+    //upload on cloudinary
+    return await cloudinary.uploader.upload(filePath, {
+      resource_type: "auto",
+    });
+  } catch (error) {
+    fs.unlinkSync(filePath)
+    return null;
+  }
+};
+
+export { uploadOnCloudinary}
